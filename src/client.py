af526f8f621e59defc449afcbaec7f1605c052ea
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2022-06-01 00:03:57 +0200

    added control for the creation of the socket, added while true and try-finally

--- src/client.py
+++ src/client.py
@@ -1,18 +1,16 @@
+from fileinput import filename
 import socket as sk
 import time
+import sys
 import ClientLibrary
 
-sock = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
-print("Client socket creation..")
-server_address = ('localhost', 10000)
-#aggiungere se c'è errore
-# try:
-#     socket.gethostbyname(sys.argv[1])
-# except socket.error:
-#     print("Invalid host name. Exiting. Next time enter in proper format.")
-#     sys.exit()
-
-message=input("\nWhat do you want to do? \n- get 'file_name' \n-put 'file_name' \n-list \n:")
+try:
+    sock = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
+    print("Client socket creation..")
+    server_address = ('localhost', 10000)
+except sock.error:
+    print("Failed to create socket.")
+    sys.exit(1)
 
 # MessClient = message.encode('utf-8')
 #     try:
@@ -21,15 +19,29 @@ message=input("\nWhat do you want to do? \n- get 'file_name' \n-put 'file_name' 
 #         print(
 #             "Error. Port numbers are not matching. Exiting. Next time please enter same port numbers.")
 #         sys.exit()
+while True:
+    fileName=input("File name: ")
+    text=input("\nWhat do you want to do with %s? \n- get 'file_name' \n-put 'file_name' \n-list \n: " % fileName)
 
-try:
-    ClientLibrary.Send()
-    print('waiting to receive')
-    data, server = sock.recvfrom(4096)
-    time.sleep(2)
-    print('received message "%s"' % data.decode('utf8'))
-except Exception as info:
-    print(info)
-finally:
-    print('closing socket')
-    sock.close()
+    try:
+        if text == "get":
+            ClientLibrary.ClientGet(sock, fileName)
+        elif text == "put":
+            ClientLibrary.ClientPut(fileName, sock, server_address)
+        elif text == "list":
+            ClientLibrary.ListClient(sock)
+    finally:
+        print('closing socket')
+        sock.close()
+
+# try:
+#     ClientLibrary.Send()
+#     print('waiting to receive')
+#     data, server = sock.recvfrom(4096)
+#     time.sleep(2)
+#     print('received message "%s"' % data.decode('utf8'))
+# except Exception as info:
+#     print(info)
+# finally:
+#     print('closing socket')
+#     sock.close()
