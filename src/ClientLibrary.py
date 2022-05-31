d0bfc06b511593817eb061a9658b37e33245e055
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2022-05-31 20:39:37 +0200

    changed ClientGet and added ClientList

--- src/ClientLibrary.py
+++ src/ClientLibrary.py
@@ -3,12 +3,30 @@ import sys
 
 sizeBuffer = 4096
 
-def Send(message, server_address, sock):
+def SendClient(message, server_address, sock):
     print('sending message..')
     sock.sendto(message.encode(), server_address)
 
-def Get(sock):
+def ClientGet(message,sock):
     print('waiting to receive..')
-    data, server = sock.recvfrom(sizeBuffer)
-    print('received message "%s"' % data.decode('utf8'))
+    try:
+        data, server = sock.recvfrom(sizeBuffer)
+        if "Error" in data(sock).decode():
+            print("File doesn't exist")
+            return
+        f = open((message.slipt())[1], "w")
 
+    except ConnectionResetError:
+        print("Error. Port numbers not matching. Exiting. Next time enter same port numbers.")
+        sys.exit()
+    except:
+        print("Timeout or some other error")
+        sys.exit()
+        
+    print('received message "%s"' % data.decode('utf8'))
+    if len(data.decode('utf8')) < 30:
+
+
+def ListClient(sock):
+    data = sock.recvfrom(sizeBuffer)
+    print(data(sock).decode())
