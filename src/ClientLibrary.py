4548d62af4e3549b93bdd33ab6b51352b4a8da10
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2022-05-31 19:12:09 +0200

    added function Get in ClientLibrary.py

--- src/ClientLibrary.py
+++ src/ClientLibrary.py
@@ -7,5 +7,8 @@ def Send(message, server_address, sock):
     print('sending message..')
     sock.sendto(message.encode(), server_address)
 
-
+def Get(sock):
+    print('waiting to receive..')
+    data, server = sock.recvfrom(sizeBuffer)
+    print('received message "%s"' % data.decode('utf8'))
 
