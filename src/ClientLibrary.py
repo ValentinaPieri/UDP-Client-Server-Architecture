f8d5318476957ea777b0aeac8d532de03e43702c
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2022-06-02 16:50:26 +0200

    changed names of the functions and changed list_client by adding a for

--- src/ClientLibrary.py
+++ src/ClientLibrary.py
@@ -1,21 +1,20 @@
-from distutils import filelist
 import os
 import sys
 
 sizeBuffer = 4096
 
-def SendClient(message, server_address, sock):
+def client_send(message, server_address, sock):
     print('sending message..')
     sock.sendto(message.encode(), server_address)
 
-def ClientGet(message,sock):
+def client_get(message,sock):
     print('waiting to receive..')
     try:
         data, server = sock.recvfrom(sizeBuffer)
         if "Error" in data(sock).decode():
             print("File doesn't exist")
             return
-        f = open((message.slipt())[1], "w")
+        f = open(message, "w")
 
     except ConnectionResetError:
         print("Error. Port numbers not matching. Exiting. Next time enter same port numbers.")
@@ -25,32 +24,12 @@ def ClientGet(message,sock):
         sys.exit()
         
     print('received message "%s"' % data.decode('utf8'))
-
-def ServerGet(filename,sock):
-    filename, addr = sock.recvfrom(sizeBuffer)
-    filename = filename.decode()
-    f = open(filename,'w')
-
-    data, addr = sock.recvfrom(sizeBuffer)
     f.write(data.decode())
 
-    print("Received a file from "+str(addr)+": "+filename)
-    f.close()
+def list_client(sock,server_address):
+    sock.sendto("list".encode(), server_address)
 
-    reply = 'File has been received!'
-    sock.sendto(reply.encode(), addr)
-
-def ListClient(sock):
-    data = sock.recvfrom(sizeBuffer)
-    print(data(sock).decode())
-
-def ListServer(sock,client_address):
-    path = os.getcwd()
-    F = os.listdir(path)
-    filelist = []
-    for file in F:
-        filelist.append(file)
-    filelistStr = str(filelist)
-    filelistEn = filelistStr.encode('utf-8')
-    sock.sendto(filelistEn, client_address)
-    print("List sent from Server")
+    num_files = int(sock.recvfrom(sizeBuffer)[0].decode())
+    for i in range(num_files):
+        data = sock.recvfrom(sizeBuffer)[0]
+        print(data.decode())
